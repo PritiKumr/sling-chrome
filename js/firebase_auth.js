32f1b56e526a1c1e4d0d5ca5cf0dfbfcec52a24e
Steve Robinson <steve.rob@me.com>
2017-04-20 00:41:56 +0530

    added google auth; persisting registrationID (notification token) in Firebase after successful auth

---
+++ js/firebase_auth.js
@@ -0,0 +1,57 @@
+var config = {
+  apiKey: "AIzaSyB8b0KoIr3kDuZpLmJrH-lk8PGLdn1W_VE",
+  authDomain: "sling-3ccb3.firebaseapp.com",
+  databaseURL: "https://sling-3ccb3.firebaseio.com",
+  projectId: "sling-3ccb3",
+  storageBucket: "sling-3ccb3.appspot.com",
+  messagingSenderId: "1005011976091"
+};
+firebase.initializeApp(config);
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var signInButton = document.getElementById("google-signin");
+  var currentUserDetails = document.getElementById("current-user");
+
+  firebase.auth().onAuthStateChanged(function(currentUser) {
+    signInButton.style.display = "none";
+    currentUserDetails.style.display = "none";
+    if (currentUser) {
+      currentUserDetails.getElementsByClassName('user-name')[0].innerHTML = currentUser.displayName;
+      currentUserDetails.getElementsByClassName('user-email')[0].innerHTML = currentUser.email;
+      currentUserDetails.style.display = "inline-block";
+
+      updateNotificationTokensForUser(currentUser);
+
+    } else {
+      signInButton.style.display = "inline-block";
+      signInButton.addEventListener('click', function () {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+
+        firebase.auth().signInWithPopup(provider).catch(function(error) {
+          alert(error.message);
+          console.log(error);
+        });
+      });
+    }
+  });
+});
+
+var updateNotificationTokensForUser = function (user) {
+  chrome.storage.local.get("registered", function(result) {
+    if (!result["registered"])
+      return;
+
+    chrome.storage.local.get("registrationId", function(registrationId) {
+      chrome.instanceID.getID(function (instanceId) {
+        firebase
+          .database()
+          .ref("users/" + 
+                user.uid + 
+               "/readerDevices/" + 
+               instanceId)
+          .set({notificationToken: registrationId});
+      })
+    });
+  });
+}
--- src/bg/background.js
+++ src/bg/background.js
@@ -1,12 +1,4 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
 function registerCallback(registrationId) {
-  console.log('registered', registrationId);
   if (chrome.runtime.lastError) {
     // When the registration fails, handle the error and retry the
     // registration later.
